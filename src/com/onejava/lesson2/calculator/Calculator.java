057d371fce325952cf85809d3ffc2fbfb0260454
OlegZuev <olegase123@gmail.com>
2018-07-07 11:51:56 +0500

    New Fills

--- src/com/onejava/lesson2/calculator/Calculator.java
+++ src/com/onejava/lesson2/calculator/Calculator.java
@@ -1,63 +1,40 @@
 package com.onejava.lesson2.calculator;
 
 public class Calculator {
-	private int num1;
-	private int num2;
-	private char mathOperation;
-	private int result;
+    private int result;
+    private String[] mathExpression;
 
-	public int getNum1() {
-		return num1;
-	}
-	
-	public void setNum1(int num1) {
-		this.num1 = num1;
-	}
-	
-	public int getNum2(){
-		return num2;
-	}
-	
-	public void setNum2(int num2) {
-		this.num2 = num2;
-	}
-	
-	public char getMathOperation() {
-		return mathOperation;
-	}
-	
-	public void setMathOperation(char mathOperation) {
-		this.mathOperation = mathOperation;
-	}
-	
-	public int getResult() {
-		return result;
-	}
-	
-	public void calculate() {
-		switch(mathOperation) {
-			case '+':
-				result = num1 + num2;
-				break;
-			case '-':
-				result = num1 - num2;
-				break;
-			case '/':
-				result = num1 / num2;
-				break;
-			case '*':
-				result = num1 * num2;
-				break;
-			case '^':
-				int numTemp = num1;
-				for (int i = 1; i < num2; i++) {
-					num1 *= numTemp;
-				}
-				result = num1;
-				break;
-			case '%':
-				result = num1 % num2;
-				break;
-		}
-	}
+    public int getResult() {
+        return result;
+    }
+
+    public void setMathExpression(String mathExpression) {
+        this.mathExpression = mathExpression.split(" ");
+    }
+
+    public void calculate() {
+        int num1 = Integer.parseInt(mathExpression[0]);
+        int num2 = Integer.parseInt(mathExpression[2]);
+
+        switch (mathExpression[1]) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = Math.subtractExact(num1, num2);
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            case "*":
+                result = Math.multiplyExact(num1, num2);
+                break;
+            case "^":
+                result = (int) Math.pow(num1, num2);
+                break;
+            case "%":
+                result = (int) Math.IEEEremainder(num1, num2);
+                break;
+        }
+    }
 }
