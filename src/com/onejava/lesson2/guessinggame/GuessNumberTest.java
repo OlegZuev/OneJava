c54b3f9c316ae364bcb1f2e002142224301f3abf
OlegZuev <olegase123@gmail.com>
2018-07-08 12:52:52 +0500

    Модификация угадайки 1.2.1

--- src/com/onejava/lesson2/guessinggame/GuessNumberTest.java
+++ src/com/onejava/lesson2/guessinggame/GuessNumberTest.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * в конце игры выведите сообщение "Игрок " + имя + " угадал число " + число + " с " + номер + " попытки"
  * если игроки не угадали число за 10 попыток, то отобразите сообщение: "У " + имя + " закончились попытки"
  *
- * @version 1.2
+ * @version 1.2.1
  */
 
 public class GuessNumberTest {
@@ -50,26 +50,27 @@ public class GuessNumberTest {
                     winnerSecond = true;
                 }
             }
-            System.out.println("Ваши числа: ");
 
-            int k;
-            for (k = 0; k < pl1.getI(); k++) {
-                System.out.println(pl1.getPlayerNumber(k) + " " + pl2.getPlayerNumber(k));
+            System.out.print("Числа игрока " + pl1.getName() + ": ");
+            for (int k = 0; k <= pl1.getI(); k++) {
+                System.out.print(pl1.getPlayerNumber(k) + " ");
+            }
+
+            System.out.print("\n" + "Числа игрока " + pl2.getName() + ": ");
+            for (int k = 0; k <= pl2.getI(); k++) {
+                System.out.print(pl2.getPlayerNumber(k) + " ");
             }
 
             if (winnerFirst) {
-                System.out.println(pl1.getPlayerNumber(k));
-                System.out.println("Игрок " + pl1.getName() + " угадал число " + guessN.getGuessNumber() + " с " +
+                System.out.print("\n" +"Игрок " + pl1.getName() + " угадал число " + guessN.getGuessNumber() + " с " +
                                    (pl1.getI() + 1) + " попытки." + "\n" +
                                    "Хотите продолжить? [да/нет]: ");
             } else if (winnerSecond) {
-                System.out.println(pl1.getPlayerNumber(k) + " " + pl2.getPlayerNumber());
-                System.out.println("Игрок " + pl2.getName() + " угадал число " + guessN.getGuessNumber() + " с " +
+                System.out.print("\n" +"Игрок " + pl2.getName() + " угадал число " + guessN.getGuessNumber() + " с " +
                                    (pl2.getI() + 1) + " попытки." + "\n" +
                                    "Хотите продолжить? [да/нет]: ");
             } else {
-                System.out.println(pl1.getPlayerNumber(k) + " " + pl2.getPlayerNumber());
-                System.out.println(pl1.getName() + " и " + pl2.getName() + ", у вас закончились попытки." + "\n" +
+                System.out.print("\n" + pl1.getName() + " и " + pl2.getName() + ", у вас закончились попытки." + "\n" +
                                    "Хотите продолжить? [да/нет]: ");
             }
 
