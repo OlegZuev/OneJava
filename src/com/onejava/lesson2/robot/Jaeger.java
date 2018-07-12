9517aff57270f357d8d9b98d378cdc426d257052
OlegZuev <olegase123@gmail.com>
2018-07-12 20:23:55 +0500

    Добавил пару методов.

--- src/com/onejava/lesson2/robot/Jaeger.java
+++ src/com/onejava/lesson2/robot/Jaeger.java
@@ -1,54 +1,81 @@
 package com.onejava.lesson2.robot;
 
+import java.util.Random;
+
 public class Jaeger {
-	private String name;
-	private String mark;
-	private String status;
-	private String weapon;
-	private int height;
+    private String name;
+    private String mark;
+    private String status;
+    private String weapon;
+    private int height;
+    private int durability = 100;
 
-	public Jaeger() {}
+    public Jaeger() {
+    }
 
-	public Jaeger(String name, String mark, String status, String weapon, int height) {
-		this.name = name;
-		this.mark = mark;
-		this.status = status;
-		this.weapon = weapon;
-		this.height = height;
-	}
+    public Jaeger(String name, String mark, String status, String weapon, int height) {
+        this.name = name;
+        this.mark = mark;
+        this.status = status;
+        this.weapon = weapon;
+        this.height = height;
+    }
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getMark() {
-		return mark;
-	}
-	public void setMark(String mark) {
-		this.mark = mark;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    public String getMark() {
+        return mark;
+    }
 
-	public String getWeapon() {
-		return weapon;
-	}
-	public void setWeapon(String weapon) {
-		this.weapon = weapon;
-	}
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
 
-	public int getHeight() {
-		return height;
-	}
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getDurability() {
+        return durability;
+    }
+
+    public void setDurability(int durability) {
+        this.durability = durability;
+    }
+
+    public void attack(Jaeger jaeger) {
+        if (!jaeger.evade()) {
+            jaeger.setDurability(getDurability() - 10);
+        }
+    }
+
+    public boolean evade() {
+        return new Random().nextBoolean();
+    }
 }
--- src/com/onejava/lesson2/robot/JaegerTest.java
+++ src/com/onejava/lesson2/robot/JaegerTest.java
@@ -19,5 +19,20 @@ public class JaegerTest {
 						   "Status: " + jaeger2.getStatus() + "\n" +
 						   "Weapon: " + jaeger2.getWeapon() + "\n" +
 						   "Height: " + jaeger2.getHeight());
+		fight(jaeger1, jaeger2);
 	}
+
+    public static void fight(Jaeger jaeger1, Jaeger jaeger2) {
+        while (jaeger1.getDurability() > 0) {
+            jaeger1.attack(jaeger2);
+
+            if (jaeger2.getDurability() <= 0) {
+                System.out.println(jaeger2.getName() + " победил.");
+                return;
+            }
+
+            jaeger2.attack(jaeger1);
+        }
+        System.out.println(jaeger1.getName() + " победил.");
+    }
 }
